17237842f3a447a81d16d5c023da0b09e31159a4
user333 <zlojdurak998@gmail.com>
2026-02-03 17:58:47 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -25,3 +25,43 @@ if (tasks === 0) {
   console.log("Много задач");
 }
 
+if (title === "") {
+  console.log("Заголовок пустой");
+} else {
+  console.log("Заголовок:" + title);
+}
+
+let  = 2;
+
+if (tasks === 0) {
+  console.log("Список пуст, можно отдыхать");
+} else if (tasks > 0 && tasks <= 3) {
+  console.log("Есть задачи для выполнения");
+} else if (tasks > 3) {
+  console.log("Список задач переполнен");
+}
+
+//урок 3
+
+function summa(a, b) {
+  return a + b;
+}
+
+console.log(summma(2, 3))
+console.log(summma(8, 34))
+
+function isTaskDone(status) {
+  return status === "выполнена";
+}
+
+let result = isTaskDone("выполнена");
+console.log(result);
+
+function taskSummery(total, done) {
+  let active = total - done;
+  return (
+    "Всего: " + total + " | Активных: " + active + " |Выполненных: " + done
+  );
+}
+
+console.log(taskSummary(8, 3))
